4998269dff7f07d03b39b9266fb81988625b08e4
davemflick <davemflick@yahoo.com>
2017-01-20 16:20:21 -0500

    all finished I with terrible style

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,12 @@
- var workTime = document.getElementById('workTime').value;
+var workTime = document.getElementById('workTime').value;
 var breakTime = document.getElementById('breakTime').value;
-// var timer = document.getElementById('timer').innerHTML;
-// var startStop = document.getElementById('startStop');
-// var curDate = new Date();
-// var sec = curDate.getSeconds();
-// var min = curDate.getMinutes();
-// var hour = curDate.getHours();
-// var curTime = new Object({});
+var $timeDisplay = document.getElementById('timer');
+var audio = new Audio('sounds/end.mp3')
 
 //Allows the Work Time and Break Time to be manipulated by 1.
 document.getElementById('workUp').onclick = function(){
 document.getElementById('workTime').stepUp();
-workTime++;
+workTime++
 }
 document.getElementById('workDown').onclick = function(){
  document.getElementById('workTime').stepDown();
@@ -28,70 +23,56 @@ document.getElementById('breakDown').onclick = function(){
 
 
 //Start of Timer Functionallity
-var timer2 = new Timer({
-    tick : 1,
-    ontick : function (sec) {
-        document.getElementById('timer').innerHTML = Math.round(sec/1000);;
-    },
-    onstart : function() {
-        document.getElementById('timer').innerHTML = breakTime;
-    }
-});
-
-timer2.on('end', function () {
-    document.getElementById('endNotice').innerHTML = 'Break time ended';
-    document.getElementById('timer').innerHTML = 0;
-    timer2.off();
-});
 
 var timer = new Timer({
     tick : 1,
     ontick : function (sec) {
-        document.getElementById('timer').innerHTML = Math.round(sec/1000);;
+        var mins = Math.floor(sec/60000);
+        var secs = Math.floor(((sec % 60000)/1000).toFixed(0));
+        $timeDisplay.innerHTML = secs !== 60 ? mins + ':' + (secs <10 ? '0' : '') + secs : mins+1 + ':00';
     },
     onstart : function() {
-        document.getElementById('timer').innerHTML = workTime;
+        $timeDisplay.innerHTML = "Here We Go!";
     }
 });
 
 timer.on('end', function () {
     document.getElementById('endNotice').innerHTML = 'Work time ended, break time starts';
-    timer2.start(breakTime);
+    audio.play();
+    timer2.start(breakTime*60);
 });
 
+var timer2 = new Timer({
+    tick : 1,
+    ontick : function (sec) {
+        var mins = Math.floor(sec/60000);
+        var secs = Math.floor(((sec % 60000)/1000).toFixed(0));
+        $timeDisplay.innerHTML = secs !== 60 ? mins + ':' + (secs <10 ? '0' : '') + secs : mins+1 + ':00';;
+    },
+    onstart : function() {
+        $timeDisplay.innerHTML = 'Break Begins!';
+    }
+});
 
+timer2.on('end', function () {
+    document.getElementById('endNotice').innerHTML = 'Break time ended';
+    $timeDisplay.innerHTML = 0;
+    audio.play();
+    timer2.off();
+});
 
 document.getElementById('startStop').onclick = function(){
-  timer.start(workTime);
-};
+  if(timer.getStatus() === 'initialized' && timer2.getStatus() !== 'started' || timer.getStatus() === 'initialized' && timer2.getStatus() !== 'paused'){
+    timer.start(workTime*60);
+  } else if (timer.getStatus() === 'started') timer.pause();
+  else if(timer.getStatus() === 'paused') timer.start();
+  else if(timer2.getStatus() === 'started') timer2.pause();
+  else if(timer2.getStatus() === 'paused') timer2.start();
+}
 
-
-
-// BASIC FRAMEWORK FROM API
-
-// var timer = new Timer({
-//     tick : 1,
-//     ontick : function (sec) {
-//         console.log('interval', sec);
-//     },
-//     onstart : function() {
-//         console.log('timer started');
-//     }
-// });
-
-
-
-// defining options using on
-// timer.on('end', function () {
-//     console.log('timer ended');
-//     this.start(4).off('end');
-// });
-
-
-
-
-
-
+document.getElementById('reset').onclick = function(){
+ location.reload();
+}
 
 
 
